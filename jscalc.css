17601858823a067e6d85481602f35a9b9e383a2a
Ashley <atan77@gmail.com>
2015-12-27 11:16:45 +1100

    Javascript calculator css
    initial css styling

---
+++ jscalc.css
@@ -0,0 +1,129 @@
+h1 {
+  font-family: arial;
+}
+.calculator {
+  text-align: center;
+  padding-top: 1em;
+  width: 500px;
+  height: 670px;
+  margin: 0 auto;
+}
+.clicker {
+  width: 20%;
+  height: 9%;
+  border: 1px solid #4B4657;
+  border-top: 2px solid #7c748f;
+  background-image: -webkit-linear-gradient( top , #4B4657, #332f3b);
+  background-image: linear-gradient(to bottom, #4B4657, #332f3b);
+  color: white;
+  font-size: 2.2em;
+  box-shadow: inset 0 1px 4px #7c748f, inset 0 -4px 20px #332f3b, 0 3px 1px 0 #332f3b;
+}
+.clicker-wide{
+  width: 40.9%;
+  height: 9%;
+  border: 1px solid #4B4657;
+  border-top: 2px solid #7c748f;
+  background-image: -webkit-linear-gradient( top , #4B4657, #332f3b);
+  background-image: linear-gradient(to bottom, #4B4657, #332f3b);
+  color: white;
+  font-size: 2.2em;
+  box-shadow: inset 0 1px 4px #7c748f, inset 0 -4px 20px #332f3b, 0 3px 1px 0 #332f3b;
+}
+.clicker-wide-tall{
+  width: 40.8%;
+  height: 18%;
+  border: 1px solid #4B4657;
+  border-top: 2px solid #7c748f;
+  background-image: -webkit-linear-gradient( top , #4B4657, #332f3b);
+  background-image: linear-gradient(to bottom, #4B4657, #332f3b);
+  color: white;
+  font-size: 2.2em;
+  text-align: center;
+  word-wrap: break-word;
+  box-shadow: inset 0 1px 4px #7c748f, inset 0 -4px 20px #332f3b, 0 3px 1px 0 #332f3b;
+}
+#tracker {
+  background-color: #D1F0FF;
+  border-bottom: 1px solid #D1F0FF;
+  border-top: 1px solid #BCD8E6;
+  border-right: 1px solid #BCD8E6;
+  border-left: 1px solid #BCD8E6;
+  line-height: 40px;
+  width: 82.5%;
+  font-size: 2em;
+  padding-right: 10px;
+}
+#output {
+  background-color: #D1F0FF;
+  border-bottom: 1px solid #BCD8E6;
+  border-top:  1px solid #D1F0FF;
+  border-right: 1px solid #BCD8E6;
+  border-left: 1px solid #BCD8E6;
+  line-height: 80px;
+  width: 82.5%;
+  font-size: 4em;
+  padding-right: 10px;
+  margin-bottom: 20px;
+}
+
+.connect-style {
+  position: fixed;
+  bottom: 0;
+  font-size: 14px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #222222;
+  color: #777;
+  vertical-align: middle;
+  padding: 12.5px;
+  /*    border-style: solid;*/
+  width: 100vw;
+}
+
+.socialbottom {
+  text-align: center;
+  border-radius: 25px;
+  vertial-align: 0px;
+  /*    border-style: solid;*/
+}
+
+.fa-twitter {
+  color: #55acee;
+}
+
+.fa-twitter:hover {
+  color: #fff;
+}
+
+.fa-github {
+  color: #333;
+}
+
+.fa-github:hover {
+  color: #fff;
+}
+
+.fa-fire {
+  color: #4a2b0f;
+}
+
+.fa-fire:hover {
+  color: #fff;
+}
+
+.fa-codepen {
+  color: #000;
+}
+
+.fa-codepen:hover {
+  color: #fff;
+}
+
+.fa-linkedin {
+  color: #0075b2;
+}
+
+.fa-linkedin:hover {
+  color: #fff;
+}
